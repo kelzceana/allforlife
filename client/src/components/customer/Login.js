098b55cc8256dd49f17406a9d6a06ceeade4d30c
Kelechi Obikaonu <71353368+kelzceana@users.noreply.github.com>
2021-02-12 10:34:47 -0500

    Update Login.js

--- client/src/components/customer/Login.js
+++ client/src/components/customer/Login.js
@@ -2,20 +2,15 @@ import React from "react";
 import { useState, useEffect} from "react";
 import axios from "axios";
 import "./Login.css";
-<<<<<<< HEAD
 import { Redirect } from "react-router-dom";
-=======
->>>>>>> 9c718e73a37e0c31af401e6fcd16c41f434d300d
+
 
 
 export default function Login(props) {
 
     const [formValues, setFormValues] = useState({
-<<<<<<< HEAD
         userName: "",
-=======
         email: "",
->>>>>>> 9c718e73a37e0c31af401e6fcd16c41f434d300d
         password: ""
     });
     const [error, setError] = useState("");
@@ -29,7 +24,6 @@ export default function Login(props) {
 
     const fetchUser = () => {
         if(formValues.name === "" || formValues.password === "") {
-<<<<<<< HEAD
             setError("userName or password cannot be blank !")
         } else {
             axios.post(`http://localhost:8010/api/login`,
@@ -39,7 +33,6 @@ export default function Login(props) {
                     } else {
                         setError("");
                         console.log(res.data)
-=======
             setError("Email or password cannot be blank !")
         } else {
             axios.post(`http://localhost:8010/api/login`,
@@ -48,7 +41,6 @@ export default function Login(props) {
                     setError("Email or password is incorrect !")
                     } else {
                         setError("");
->>>>>>> 9c718e73a37e0c31af401e6fcd16c41f434d300d
                         props.setUser(res.data)
                         setLoggedIn(true);
                 } 
@@ -56,11 +48,10 @@ export default function Login(props) {
         }
     }
 
-<<<<<<< HEAD
+
     return !loggedIn ?(
-=======
+
     return (
->>>>>>> 9c718e73a37e0c31af401e6fcd16c41f434d300d
         <div className="login-container">
             <div className="login">  
                 <form className="login-form" onSubmit={event => event.preventDefault()} >
@@ -68,13 +59,11 @@ export default function Login(props) {
                     {error && <div className="alert-error">
                     {error}
                     </div>}
-<<<<<<< HEAD
                     <h3>Username:</h3>
                     <input type="text" name="userName" value ={formValues.userName} onChange = {handleChange}  placeholder="Username"/>
-=======
+
                     <h3>Email:</h3>
                     <input type="text" name="email" value ={formValues.email} onChange = {handleChange}  placeholder="Email"/>
->>>>>>> 9c718e73a37e0c31af401e6fcd16c41f434d300d
                     <h3>Password:</h3>
                     <input type="password" name="password" value= {formValues.password} onChange = {handleChange}  placeholder="Password"/>
                     <br/>
@@ -82,11 +71,9 @@ export default function Login(props) {
                 </form>
             </div>
         </div>
-<<<<<<< HEAD
     ): <Redirect to='/'></Redirect>;
 }
-=======
     )
     
 }
->>>>>>> 9c718e73a37e0c31af401e6fcd16c41f434d300d
+
