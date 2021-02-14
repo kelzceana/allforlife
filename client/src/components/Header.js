1b0dc7acb712d8ca5f0e957ac54b117e911b666e
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-13 19:17:41 -0500

    modified header to read from token

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,29 +1,32 @@
 import React from "react";
-
+import { Redirect } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 
-const logout = () => {
-  localStorage.removeItem("token")
-}
-
 export default function Header(props) {
-  const {user,setUser } = props;
-  console.log(user, "hello user")
+  
+  const handleClick = (evt) => {
+    evt.preventDefault();
+    localStorage.setItem("token", "");
+    props.setUser(undefined);
+    <Redirect to='/login'></Redirect>
+  }
+
+  console.log(props.user, "props user")
     return (
       <nav className="nav-container">
           <div className="nav-div-image">
             <img src="./images/logo.png" />
           </div>
-         {!user && <div className="nav-links">
+         {props.user === undefined && <div className="nav-links">
             <Link to="/login" className="nav-link-id">Login</Link> 
             <Link to="/register" className="nav-link-id">Register</Link>
           </div>
           }
-          {user && <div className="nav-links">
-            <span className="nav-link-id"> Hi {user.user.userName}</span>
-            <span className="nav-link-id" onClick={() => setUser(null)}>Logout</span>
+          {props.user !== undefined && <div className="nav-links">
+            <span className="nav-link-id"> Hi {props.user.userName}</span>
+            <span className="nav-link-id" onClick={(evt) => handleClick(evt)}>Logout</span>
           </div>
           }
       </nav>
