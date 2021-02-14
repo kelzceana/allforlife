6f2978cafee3044e0dd0761e4bcb79670c9aa436
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-02-13 19:17:01 -0500

    set user to read from local storage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from "react-router-dom";
 import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Header';
@@ -19,26 +20,27 @@ import {decodeUser} from '../util/index'
 
 function App() {
  const userFromStorage = decodeUser()
-  const [user,setUser]= useState(userFromStorage || null);
-  console.log(user, "app userrrrr")
+ 
+  const [loggeduser,setUser]= useState(userFromStorage);
+  
   return (
     <Router>
-      <Header user={user} setUser={setUser}/>
+      <Header user={loggeduser} setUser={setUser}/>
       <Switch>
         <Route path="/customerlogin">
           <Login setUser={setUser} /> 
         </Route>
         <Route path="/customerregister">
-          <Register setUser={setUser} />
+         <Register setUser={setUser} />
         </Route>
         <Route path="/register">
-          {!user && <RegisterDecision setUser={setUser} />}
+          {<RegisterDecision setUser={setUser} />}
         </Route>
         <Route path="/login">
-          {!user && <LoginDecision setUser={setUser} />}
+          {<LoginDecision setUser={setUser} />}
         </Route>
         <Route path="/" exact>
-          <PostAd  user={user.user.id}/>
+          {loggeduser && <PostAd  user={loggeduser}/>}
         </Route>
       </Switch>
       <Footer />
