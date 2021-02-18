7bbb1f0348391e03f82c7e79d99e5893d75f95d8
MegaMind <ramyaa1431@gmail.com>
2021-02-18 00:39:47 -0500

    add history method for routing and nav bar routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,50 @@
 import React from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { FcHome } from "react-icons/fc"
 import "./Header.css";
 
 export default function Header(props) {
+
+  //local variable to use history method
+  let history = useHistory();
+  
   const { user,setUser } = props;
   const handleClick =(event) => {
     event.preventDefault();
     localStorage.setItem("token", "");
     setUser(undefined);
-    props.setLoggedIn(false);
+    history.push('/');
   }
 
     return (
       <nav className="nav-container">
-          <div className="nav-div-image">
-            <img src="/image/logo.png" alt="logo" />
+         <div className="nav-div-image">
+            <Link to="/"><img src="/image/logo.png" alt="logo" /></Link>           
           </div>
+          <div className="nav-links">
           {user && <div className="nav-links">
-            <span className="nav-link-id"> Hi {user.userName}</span>
-            <Link to='/postAd'>PostAd</Link>
             <span className="nav-link-id" onClick={(event) => handleClick(event)}>Logout</span>
           </div>
           }
+          { user && 
+            <div className="nav-links">
+              {user.type === 'customer' ?
+              <Link className="nav-link-id" to="/customer/dashboard">DashBoard</Link>:
+              <Link className="nav-link-id" to="/provider/dashboard">DashBoard</Link> }
+            </div>
+          }  
+          <div className="nav-home-icon">
+            <Link to="/">
+              <FcHome  style={{height:'2em', 
+                width:'2em', 
+                paddingBottom:'0.3em',
+                alignContent:'center'
+                }}
+              />
+            </Link>
+          </div>
+          </div>
+  
       </nav>
     );
 }
